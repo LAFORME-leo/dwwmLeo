8d321fed4fe999c6ff2e22775371b28be0c6d465
ThierryB <thierry-bru@laposte.net>
2024-02-08 08:42:53 +0100

    amélioration et relecture stagiaire

--- html/js/cookies/aufilducode/cookies.js
+++ html/js/cookies/aufilducode/cookies.js
@@ -1,4 +1,7 @@
 document.cookie='mode=light;max-age='+30*24*60*60+";secure";
+// 'max-age' permet de donner un "compte à rebours" pour l'attribut à laquelle on l'attache
+// 'max-age' est un attribut global
+// Par exemple, ici, le cookie pour le mode=light durera 1 mois (=30 jours*24 heures*60 minutes*60 secondes)
 // document.cookie='langue=french;max-age='+30*24*60*60;
 // document.cookie='lang=;max-age=1';
 // document.cookie='langu=;max-age=1';
@@ -6,8 +9,13 @@ document.cookie='mode=light;max-age='+30*24*60*60+";secure";
 function getCookieListInArray(pCookies) {
     let listeCookies = new Array();
     pCookies.split(';').forEach(element => {
+        // .split mets toutes les données de pCookies dans un tableau, puis on parcours le tableau entier avec forEach
         let temporaire = element.split('=');
-        listeCookies[temporaire[0].trim()] = temporaire[1].trim();
+        // On met en variable une chaine qui va séparer tous les éléments du tableau
+        let cle =temporaire[0].trim();
+        let valeur=temporaire[1].trim();
+        listeCookies[cle] = valeur;
+        // Permet de retourner toutes les valeurs du split (tableau) selon l'index, avec chaques ensembles séparés par ";".
     });
     return listeCookies;
 }
--- html/js/webstorage/aufilducode/main.js
+++ html/js/webstorage/aufilducode/main.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded',()=>{
-
+// Cette fonction permet de vérifier que tout le contenu est chargé avant d'exécuter la fonction "afficherDemanderPrenom"
     const btn_oublier = document.querySelector('#btn_oublier')
+    //On selectionne la id du boutton
     btn_oublier.addEventListener('click',()=>
     {
         sessionStorage.clear()
+        // On efface tout le contenu stocké dans le navigateur, lié au bouton avec l'id #btn_oublier
     })
+    // cela va appeler une fonction
     afficherDemanderPrenom();
 })
 
@@ -14,6 +17,8 @@ function afficherDemanderPrenom() {
     if ((sessionStorage.getItem('prenom') == '') || (sessionStorage.getItem('prenom') == null)) {
         let prenom = prompt('Quel est votre prénom?');
         sessionStorage.setItem('prenom', prenom);
+        // On place la variable "prompted" 'prenom' dans le stockage du navigateur pour l'attribut 'prenom'
+        // Contrairement à cookies, qui est sauvegardé sur le disque dur, SessionStorage utilise le navigateur pour stocker et récupérer les infos
     }
 
     else {
